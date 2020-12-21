1fbd06c6cfaf9c8c353ac16e17f00574f0ea40b2
gh2571527745 <2571527745@qq.com>
2020-12-21 17:15:19 +0800

    1，添加ARouter，2，添加lib-aop,lib-base,lib-common,lib-db,lib-network,lib-widget和moudle-home

---
+++ moudle-home/src/main/java/com/ld/module_home/HomeApp.kt
@@ -0,0 +1,9 @@
+package com.ld.module_home
+
+import android.app.Application
+
+class HomeApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- moudle-home/src/main/java/com/ld/module_home/activity/MainActivity.kt
+++ moudle-home/src/main/java/com/ld/module_home/activity/MainActivity.kt
@@ -1,13 +1,13 @@
-package com.ld.general_framework
+package com.ld.module_home.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ld.module_home.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
